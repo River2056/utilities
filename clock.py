d947816eed4f53ce0dc7a4b24ad647d1e7966b21
Kevin_Tung__ <kevin.tung@hytechtech.com>
2023-03-21 14:27:28 +0800

    + modify

--- clock.py
+++ clock.py
@@ -3,21 +3,23 @@ import art
 import curses
 from datetime import datetime
 
+
 def main():
     scr = curses.initscr()
-    rows, cols = scr.getmaxyx()
+    rows, _ = scr.getmaxyx()
 
     while True:
         try:
             scr.clear()
-            curr = (" " * 6) + datetime.now().strftime("%H:%M:%S")
+            curr = (" " * 7) + datetime.now().strftime("%H:%M:%S")
             time_txt = art.text2art(curr, font="clb8x10")
             scr.addstr(rows // 2 - 5, 0, time_txt)
             curses.napms(1000)
             scr.refresh()
-        except KeyboardInterrupt as ke:
+        except KeyboardInterrupt:
             curses.endwin()
             sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
