0b4a71bbbf9e94f56698625332dcc5a179240e12
river2056 <chen0625tung@gmail.com>
2024-02-05 15:57:11 +0800

    modified
    + modified display position, hopefully centered everytime without hardcoded value

--- clock.py
+++ clock.py
@@ -6,14 +6,14 @@ from datetime import datetime
 
 def main():
     scr = curses.initscr()
-    rows, _ = scr.getmaxyx()
+    rows, cols = scr.getmaxyx()
 
     while True:
         try:
             scr.clear()
-            curr = (" " * 7) + datetime.now().strftime("%H:%M:%S")
+            curr = (" " * ((cols // 10) - 12)) + datetime.now().strftime("%H:%M:%S")
             time_txt = art.text2art(curr, font="clb8x10")
-            scr.addstr(rows // 2 - 5, 0, time_txt)
+            scr.addstr(rows // 2 - 5, 0, time_txt)  # type: ignore
             curses.napms(1000)
             scr.refresh()
         except KeyboardInterrupt:
